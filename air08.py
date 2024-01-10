8f0e40214a06f8b7504d7209ac53dcf4a9dbd6d3
Ali-Akif <ali-akif@live.fr>
2024-01-10 20:25:02 +0100

    Update air08.py

--- air08.py
+++ air08.py
@@ -4,45 +4,42 @@ import sys
 
 # Part 1 : Function
 
-def sorted_array_blender(array, array2):
-    index = index2 = 0
-    new_array = []
-    while index < len(array) and index2 < len(array2):
-        if array[index] < array2[index2]:
-            new_array.append(array[index])
-            index += 1
+def is_sorted(lst):
+    return all(lst[i] <= lst[i+1] for i in range(len(lst) - 1))
+
+def sorted_array_blender(array1, array2):
+    index1 = index2 = 0
+    merged_array = []
+    while index1 < len(array1) and index2 < len(array2):
+        if array1[index1] < array2[index2]:
+            merged_array.append(array1[index1])
+            index1 += 1
         else:
-            new_array.append(array2[index2])
+            merged_array.append(array2[index2])
             index2 += 1
-    while index < len(array):
-        new_array.append(array[index])
-        index += 1
-    while index2 < len(array2):
-        new_array.append(array2[index2])
-        index2 += 1
-    return new_array
+    merged_array.extend(array1[index1:])
+    merged_array.extend(array2[index2:])
+    return merged_array
 
 
-# Part 2 : Error Handling
+# Part 2 : Error Handling and Slicing
 
-if len(sys.argv) < 4 and not ("fusion" in sys.argv[1:] and sys.argv[1].isdigit() and sys.argv[-1].isdigit() and sys.argv.count("fusion")) == 1:
-    print("erreur")
+terminal_arg = sys.argv[1:]
+
+if not( "fusion" in terminal_arg and "".join(terminal_arg).replace("fusion", "").isdigit() ):
+    print('error : enter arg in format : "sorted array" fusion "sorted array"')
     exit()
 
-
-# Part 3 : Slicing
-    
-terminal_arg = sys.argv[1:]
 index_fusion = terminal_arg.index("fusion")
 array1 = terminal_arg[:index_fusion]
 array2 = terminal_arg[index_fusion + 1:]
+
+if not (is_sorted(array1) and is_sorted(array2)): #Hard to put it on the same line as the other without using a try except )
+    print("error : array not sorted")
+    exit()
+
+
+# Part 3 : Resolution and Display
+
 result = sorted_array_blender(array1,array2)
-result = [str(n) for n in result]
-
-
-# Part 4 : Display
-
-print(", ".join(result))
-
-
-
+print(", ".join(map(str,result)))
