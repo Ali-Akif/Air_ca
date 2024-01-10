cf8f03b36aef0b83ea18d91eb28b6a39ed10a12a
Ali-Akif <ali-akif@live.fr>
2024-01-10 12:54:05 +0100

    Update air08.py

--- air08.py
+++ air08.py
@@ -1,13 +1,48 @@
 # BLEND SORTED ARRAY
 
-liste = [10, 30, 40]
-liste2 = [15, 29, 33]
-new_array = []
+import sys
 
-index, index2 = 0, 0
+# Part 1 : Function
+
+def sorted_array_blender(array, array2):
+    index = index2 = 0
+    new_array = []
+    while index < len(array) and index2 < len(array2):
+        if array[index] < array2[index2]:
+            new_array.append(array[index])
+            index += 1
+        else:
+            new_array.append(array2[index2])
+            index2 += 1
+    while index < len(array):
+        new_array.append(array[index])
+        index += 1
+    while index2 < len(array2):
+        new_array.append(array2[index2])
+        index2 += 1
+    return new_array
+
+
+# Part 2 : Error Handling
+
+if len(sys.argv) < 4 and not ("fusion" in sys.argv[1:] and sys.argv[1].isdigit() and sys.argv[-1].isdigit() and sys.argv.count("fusion")) == 1:
+    print("erreur")
+    exit()
+
+
+# Part 3 : Slicing
+    
+terminal_arg = sys.argv[1:]
+index_fusion = terminal_arg.index("fusion")
+array1 = terminal_arg[:index_fusion]
+array2 = terminal_arg[index_fusion + 1:]
+result = sorted_array_blender(array1,array2)
+result = [str(n) for n in result]
+
+
+# Part 4 : Display
+
+print(", ".join(result))
 
 
 
-
-
-print(new_array)
