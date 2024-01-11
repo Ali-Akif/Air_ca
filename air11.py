d26eccb88e9462a553adddff7a9d2b13a58afc9d
Ali-Akif <ali-akif@live.fr>
2024-01-11 17:12:10 +0100

    Create air11.py

---
+++ air11.py
@@ -0,0 +1,26 @@
+# PYRAMID
+
+import sys
+
+
+# Part 1 : Error Handling
+
+if not (len(sys.argv) == 3 and len(sys.argv[1]) == 1 and sys.argv[2].isdigit()):
+    print("erreur")
+    exit()
+
+
+# Part 2 : Slicing
+
+args = sys.argv[1:]
+car, column = args[0], int(args[1])
+spaces = column - 1
+lenght_line = 1
+
+
+# Part 3 : Resolution and display
+
+for i in range(1, column + 1):
+    print(f"{'' if i == column else ' ' * spaces}{car * lenght_line}")
+    spaces -= 1
+    lenght_line += 2
