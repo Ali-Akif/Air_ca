4f4b5c4f1b07f6db063e61c4d41b893d0b91e236
Ali-Akif <ali-akif@live.fr>
2024-01-11 09:11:32 +0100

    Create air10.py

---
+++ air10.py
@@ -0,0 +1,26 @@
+# READ FILE
+
+import sys
+
+
+# Part 1 : Error Handling
+
+if len(sys.argv) != 2:
+    print("Erreur : veuillez spécifier un fichier à lire.")
+    sys.exit(1)
+
+
+# Part 2 : Smol Slicing
+    
+filename = sys.argv[1]
+
+
+# Part 3 : Display
+
+try:
+    with open(filename, "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("Erreur : Le fichier spécifié n'as pas été trouvé.")
+except PermissionError:
+    print("Erreur : Permission refuséee pour ouvrir le fichier.")
