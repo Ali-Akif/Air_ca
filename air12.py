240035b8e634fb988165f88dc65097a252b84bd1
Ali-Akif <ali-akif@live.fr>
2024-01-12 22:56:46 +0100

    Create air12.py

---
+++ air12.py
@@ -0,0 +1,29 @@
+# KING OF SORT
+
+import sys
+
+
+# Part 1 : Function
+
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+        left = [x for x in array[1:] if x < pivot]
+        right = [x for x in array[1:] if x >= pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
+
+# Part 2 : Error Handling
+
+if not (len(sys.argv) > 2 and "".join(sys.argv[1:]).isdigit()):
+    print("erreur")
+    exit()
+
+
+# Part 3 : Slicing
+    
+arr = [int(n) for n in sys.argv[1:]]
+result = map(str, quicksort(arr))
+print(", ".join(result))
+
