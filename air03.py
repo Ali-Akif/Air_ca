24c6d4015515f811bd9ecc94413ff862e5f49190
Ali-Akif <ali-akif@live.fr>
2024-01-13 00:06:07 +0100

    Update air03.py

--- air03.py
+++ air03.py
@@ -13,18 +13,24 @@ if len(sys.argv) < 4:
 # Part 2 : Slicing
 
 liste = sys.argv[1:]
-impostor = "not found"
-print
+impostor = []
 
 
 # Part 3 : Resolution
 
 for object in liste:
     if liste.count(object) == 1:
-        impostor = object
+        impostor.append(object)
     
 
 # Part 4 : Display
     
-print(impostor)
+if impostor:
+    if len(impostor) > 1:
+        for i, word in enumerate(impostor):
+            print(f"{i + 1}: {word}")
+    else:
+        print(impostor[0])
+else:
+    print("not found")
 
