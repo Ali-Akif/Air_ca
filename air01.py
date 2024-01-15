6c4666a69a8b5ccc516b6b4c4f3492424d6b4ced
Ali-Akif <ali-akif@live.fr>
2024-01-15 21:30:28 +0100

    Update air01.py

--- air01.py
+++ air01.py
@@ -2,9 +2,6 @@
 
 import sys
 
-
-# Part 1 : Function
-
 def split_from_arg(string, separateur):
     tableau = []
     index_separateur = string.find(separateur)
@@ -14,22 +11,9 @@ def split_from_arg(string, separateur):
     tableau.extend([string[:index_separateur], string[index_separateur + len(separateur):]])
     return [i for i in tableau if i]
 
-
-# Part 2 : Error Handling
-
 if not len(sys.argv) == 3 or sys.argv[1] == sys.argv[2]:
     print("erreur")
     sys.exit(1)
 
-
-# Part 3 : Slicing
-    
-string = sys.argv[1]
-separateur = sys.argv[2]
-tableau = split_from_arg(string, separateur)
-
-
-# Part 4 : Display
-
-for i in tableau:
+for i in split_from_arg(sys.argv[1], sys.argv[2]):
     print(i)
