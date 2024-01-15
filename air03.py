5fcaeb03a3f168cff3fe5c84ffa36a6ecf1208e0
Ali-Akif <ali-akif@live.fr>
2024-01-15 21:30:37 +0100

    Update air03.py

--- air03.py
+++ air03.py
@@ -2,35 +2,23 @@
 
 import sys
 
-
-# Part 1 : Error Handling
+def find_intruder(liste):
+    impostor = []
+    for object in liste:
+        if liste.count(object) == 1:
+            impostor.append(object)
+    if impostor:
+        if len(impostor) > 1:
+            for i, word in enumerate(impostor):
+                print(f"{i + 1}: {word}")
+        else:
+            print(impostor[0])
+    else:
+        print("not found")
 
 if len(sys.argv) < 4:
     print("erreur")
     sys.exit(1)
 
-
-# Part 2 : Slicing
-
-liste = sys.argv[1:]
-impostor = []
-
-
-# Part 3 : Resolution
-
-for object in liste:
-    if liste.count(object) == 1:
-        impostor.append(object)
-    
-
-# Part 4 : Display
-    
-if impostor:
-    if len(impostor) > 1:
-        for i, word in enumerate(impostor):
-            print(f"{i + 1}: {word}")
-    else:
-        print(impostor[0])
-else:
-    print("not found")
+find_intruder(sys.argv[1:])
 
