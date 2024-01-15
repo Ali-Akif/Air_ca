dc1bf0232848827280b698eedda022753c6d0d94
Ali-Akif <ali-akif@live.fr>
2024-01-15 21:30:33 +0100

    Update air02.py

--- air02.py
+++ air02.py
@@ -2,25 +2,14 @@
 
 import sys
 
-
-# Part 1 : Function
-
-def join_from_wish(array, joiner):
-
+def join_from_wish(array):
+    joiner = array.pop(-1)
     for i in range(len(array) - 1):
         print(f"{array[i]}{joiner}", end = "")
     print(array[-1])
-
-
-# Part 2 : Error Handling
-    
+  
 if len(sys.argv) < 2:
     print("erreur")
     sys.exit(1)
 
-
-# Part 3 : Slicing and Resolution
-    
-terminal_arguments = sys.argv[1:]
-last_arg = terminal_arguments.pop(-1)
-join_from_wish(terminal_arguments, last_arg)
+join_from_wish(sys.argv[1:])
